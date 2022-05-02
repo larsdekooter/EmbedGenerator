ccbc504ae7709553461e28feeb20d52ff8b47689
larsdekooter <102305474+larsdekooter@users.noreply.github.com>
2022-05-02 18:39:22 +0200

    Added comments

--- code/index.js
+++ code/index.js
@@ -1,12 +1,13 @@
+//for the site
 const express = require('express');
 const bodyParser = require('body-parser');
+
 
 const app = express();
 
 const jsonParser = bodyParser.json();
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
+//for the database
 const Database = require('@replit/database');
 const db = new Database()
 
@@ -16,7 +17,7 @@ app.get('/', (request, response) => {
   
 })
 
-
+// to generate the embed in discord
 app.get(/embeds/, (request, response) => {
   const id = request.url.slice(8);
    db.get(id).then(embed => {
@@ -36,16 +37,16 @@ app.get(/embeds/, (request, response) => {
   })
   
 })
-
+//to load css and js in 'public' folder
 app.use(express.static('public'));
-
+//get info from the database
 app.get('/database/get', async (req, res) => {
   res.send(database)
 })
-
+//send info to the database
 app.post('/database/post', jsonParser, async (req, res) => {
   res.send(req.body)
   db.set(req.body.id, req.body).then(() => {})
 })
-
+//login into the host
 app.listen(8080, () => console.log('App listening at http://localhost:8080'))
