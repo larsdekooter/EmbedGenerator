177a850b82821d16a8f77419d73607ecdbabe948
larsdekooter <102305474+larsdekooter@users.noreply.github.com>
2022-05-02 18:29:54 +0200

    Id usage for shorter links

--- code/index.js
+++ code/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const jsonParser = bodyParser.json();
 
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const Database = require('@replit/database');
+const db = new Database()
 
 app.get('/', (request, response) => {
   console.log('Site opened D:')
@@ -13,23 +19,39 @@ app.get('/', (request, response) => {
 
 
 app.get(/embeds/, (request, response) => {
-  let decoded = JSON.parse(decodeURIComponent((request.url.slice(8) + '')));
-  decoded.color = `#${decoded.color}`
-  console.log(decoded)
-  response.send(`<html>
+  // let decoded = JSON.parse(decodeURIComponent((request.url.slice(8) + '')));
+  const id = request.url.slice(8);
+  console.log(id)
+   db.get(id).then(embed => {
+    response.send(`<html>
         <head>
-        <meta content="${decoded.title}" property="og:title"/>
-        <meta content="${decoded.description}" property="og:description"/>
-        <meta content="${decoded.image}" property="og:image"/>
-        <meta content="${decoded.color}" name="theme-color"/>
+        <meta content="${embed.title}" property="og:title"/>
+        <meta content="${embed.description}" property="og:description"/>
+        <meta content="${embed.image}" property="og:image"/>
+        <meta content="${embed.color}" name="theme-color"/>
         <meta name="twitter:card" content="summary_large_image">
         </head>
         <body>
-        ${JSON.stringify(decoded)}
+        ${JSON.stringify(embed)}
         </body>
-        </html>`)
+        </html>`
+  )  
+  })
+  // console.log(decoded)
+  
 })
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get('/database/get', async (req, res) => {
+  res.send(database)
+})
+
+app.post('/database/post', jsonParser, async (req, res) => {
+  res.send(req.body)
+  // database[req.body.id] = req.body;
+  // console.log(database)
+  db.set(req.body.id, req.body).then(() => {})
+})
 
 app.listen(8080, () => console.log('App listening at http://localhost:8080'))
